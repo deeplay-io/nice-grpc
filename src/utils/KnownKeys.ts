942d9dfd711069ab34371b14ba7e41c744d54812
aikoven <aikoven@deeplay.io>
2021-03-01 14:46:57 +0700

    Fix types for TypeScript 4.2
    Closes #2

--- src/utils/KnownKeys.ts
+++ src/utils/KnownKeys.ts
@@ -4,5 +4,7 @@
  * See https://stackoverflow.com/questions/51465182/typescript-remove-index-signature-using-mapped-types
  */
 export type KnownKeys<T> = {
-  [K in keyof T]: string extends K ? never : number extends K ? never : K
-} extends { [_ in keyof T]: infer U } ? U : never;
+  [K in keyof T]: string extends K ? never : K;
+} extends {[_ in keyof T]: infer U}
+  ? U & string
+  : never;
