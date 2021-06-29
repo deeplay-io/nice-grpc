dd2e80d41daadb736661f08eb59298ab963b1492
aikoven <aikoven@deeplay.io>
2021-06-29 10:55:52 +0700

    add unit test

--- packages/nice-grpc/src/__tests__/ts-proto.ts
+++ packages/nice-grpc/src/__tests__/ts-proto.ts
@@ -3,6 +3,7 @@ import {createChannel, createClient, createServer} from '..';
 import {
   DeepPartial,
   TestDefinition,
+  Test2Definition,
   TestRequest,
   TestResponse,
 } from '../../fixtures/ts-proto/test';
@@ -34,6 +35,63 @@ test('basic', async () => {
             "id": "test",
           }
         `);
+
+  channel.close();
+
+  await server.shutdown();
+});
+
+test('middleware', async () => {
+  const server = createServer();
+
+  const middlewareCalls: any[] = [];
+
+  server
+    .with(async function* (call, context) {
+      middlewareCalls.push(call);
+
+      return yield* call.next(call.request, context);
+    })
+    .add(Test2Definition, {
+      async testUnary(
+        request: TestRequest,
+      ): Promise<DeepPartial<TestResponse>> {
+        return {
+          id: request.id,
+        };
+      },
+    });
+
+  const address = `localhost:${await getPort()}`;
+
+  await server.listen(address);
+
+  const channel = createChannel(address);
+  const client = createClient(Test2Definition, channel);
+
+  await expect(client.testUnary({id: 'test'})).resolves.toMatchInlineSnapshot(`
+          Object {
+            "id": "test",
+          }
+        `);
+  expect(middlewareCalls).toMatchInlineSnapshot(`
+    Array [
+      Object {
+        "method": Object {
+          "options": Object {
+            "idempotencyLevel": "IDEMPOTENT",
+          },
+          "path": "/nice_grpc.test.Test2/TestUnary",
+        },
+        "next": [Function],
+        "request": Object {
+          "id": "test",
+        },
+        "requestStream": false,
+        "responseStream": false,
+      },
+    ]
+  `);
 
   channel.close();
 
