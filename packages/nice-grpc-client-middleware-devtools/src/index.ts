aae107184554676c212ac792084ab105bafdd0d2
segfault16 <sebastian.halder@gmx.de>
2023-10-05 06:35:11 +0200

    Add optional logging of cancelled requests

--- packages/nice-grpc-client-middleware-devtools/src/index.ts
+++ packages/nice-grpc-client-middleware-devtools/src/index.ts
@@ -1,5 +1,4 @@
 import { isAbortError } from 'abort-controller-x';
-import { Message } from 'google-protobuf';
 import {
   CallOptions,
   ClientError,
@@ -8,173 +7,210 @@ import {
   composeClientMiddleware,
 } from 'nice-grpc-common';
 
-export const devtoolsUnaryLoggingMiddleware: ClientMiddleware = async function* devtoolsLoggingMiddleware<Request, Response>(
-  call: ClientMiddlewareCall<Request, Response>,
-  options: CallOptions
-): AsyncGenerator<Response, Response | void, undefined> {
-  // skip streaming calls
-  if (call.requestStream || call.responseStream) {
-    return yield* call.next(call.request, options);
-  }
+export type DevtoolsLoggingOptions = {
+  /**
+   * Skip logging abort errors.
+   * 
+   * By default, abort errors are logged.
+   */
+  skipAbortErrorLogging?: boolean;
+};
 
-  const { path } = call.method;
-
-  let reqObj: any = undefined;
-
-  // TODO: When would it not be a jspb.Message?
-  if (call.request instanceof Message) {
-    const reqMsg = call.request as Message;
-    reqObj = reqMsg.toObject();
-  } else {
-    //eslint-disable-next-line
-    reqObj = call.request;
-    // console.warn('TODO: Request isn\'t a jspb.Message');
-  }
-
-  try {
-    const result = yield* call.next(call.request, options);
-    let resObj: any = undefined;
-    // TODO: When would it not be a jspb.Message?
-    if (result instanceof Message) {
-      const resMsg = result as Message;
-      resObj = resMsg.toObject();
-    } else {
-      //eslint-disable-next-line
-      resObj = result;
-      // console.warn("TODO: Response isn't a jspb.Message");
+export const devtoolsUnaryLoggingMiddleware: ClientMiddleware<DevtoolsLoggingOptions> =
+  async function* devtoolsLoggingMiddleware<Request, Response>(
+    call: ClientMiddlewareCall<Request, Response>,
+    options: CallOptions & Partial<DevtoolsLoggingOptions>
+  ): AsyncGenerator<Response, Response | void, undefined> {
+    // skip streaming calls
+    if (call.requestStream || call.responseStream) {
+      return yield* call.next(call.request, options);
     }
-    window.postMessage(
-      {
-        method: path,
-        methodType: 'unary',
-        request: reqObj,
-        response: resObj,
-        type: '__GRPCWEB_DEVTOOLS__',
-      },
-      '*'
-    );
-    return result;
-  } catch (error) {
-    if (error instanceof ClientError) {
+
+    // log unary calls
+    const { path } = call.method;
+    const reqObj = getAsObject(call.request);
+
+    try {
+      const result = yield* call.next(call.request, options);
+      const resObj = getAsObject(result);
       window.postMessage(
         {
-          error: {
-            code: error?.code,
-            message: `${error?.message || error}`,
-            // meta: error?.meta,
-            // methodName: error?.methodName,
-            name: error?.name,
-            // serviceName: error?.serviceName,
-            stack: error?.stack,
-          },
           method: path,
           methodType: 'unary',
           request: reqObj,
-          type: '__GRPCWEB_DEVTOOLS__',
-        },
-        '*'
-      );
-    } else if (isAbortError(error)) {
-      //eslint-disable-next-line
-      console.debug('GRPC REQUEST CANCEL', path);
-    } else {
-      //eslint-disable-next-line
-      console.error('GRPC RESPONSE FAILURE', path, error);
-    }
-
-    throw error;
-  }
-};
-
-export const devtoolsStreamLoggingMiddleware: ClientMiddleware = async function* devtoolsLoggingMiddleware<Request, Response>(
-  call: ClientMiddlewareCall<Request, Response>,
-  options: CallOptions
-): AsyncGenerator<Response, Response | void, undefined> {
-  const { path } = call.method;
-
-  let reqObj: any = undefined;
-
-  // TODO: When would it not be a jspb.Message?
-  if (call.request instanceof Message) {
-    const reqMsg = call.request as Message;
-    reqObj = reqMsg.toObject();
-  } else {
-    //eslint-disable-next-line
-    reqObj = call.request;
-  }
-
-  if (!call.responseStream && !call.requestStream) {
-    // UNARY
-    return yield* call.next(call.request, options);
-  }
-  // server streaming
-  let first = true;
-  try {
-    for await (const response of call.next(call.request, options)) {
-      // log response here
-      let resObj: any = undefined;
-      if (response instanceof Message) {
-        const resMsg = response as Message;
-        resObj = resMsg.toObject();
-      } else {
-        resObj = response;
-      }
-      if (first) {
-        window.postMessage(
-          {
-            method: path,
-            methodType: 'server_streaming',
-            request: reqObj,
-            type: '__GRPCWEB_DEVTOOLS__',
-          },
-          '*'
-        );
-        first = false;
-      }
-      window.postMessage(
-        {
-          method: path,
-          methodType: 'server_streaming',
           response: resObj,
           type: '__GRPCWEB_DEVTOOLS__',
         },
         '*'
       );
-
-      yield response;
-    }
-  } catch (error) {
-    if (error instanceof ClientError) {
-      window.postMessage(
-        {
-          error: {
-            code: error?.code,
-            message: `${error?.message || error}`,
-            // meta: error?.meta,
-            // methodName: error?.methodName,
-            name: error?.name,
-            // serviceName: error?.serviceName,
-            stack: error?.stack,
+      return result;
+    } catch (error) {
+      if (error instanceof ClientError) {
+        window.postMessage(
+          {
+            error: {
+              code: error?.code,
+              message: `${error?.message || error}`,
+              name: error?.name,
+              stack: error?.stack,
+            },
+            method: path,
+            methodType: 'unary',
+            request: reqObj,
+            type: '__GRPCWEB_DEVTOOLS__',
           },
-          method: path,
-          methodType: 'server_streaming',
-          type: '__GRPCWEB_DEVTOOLS__',
-        },
-        '*'
-      );
-    } else if (isAbortError(error)) {
-      //eslint-disable-next-line
-      console.debug('GRPC REQUEST CANCEL', path);
-    } else {
-      //eslint-disable-next-line
-      console.error('GRPC RESPONSE FAILURE', path, error);
+          '*'
+        );
+      } else if (isAbortError(error) && error instanceof Error) {
+        if (!options.skipAbortErrorLogging) {
+          window.postMessage(
+            {
+              error: {
+                code: 1,
+                message: `${error?.message || error}`,
+                name: error?.name,
+                stack: error?.stack,
+              },
+              method: path,
+              methodType: 'unary',
+              request: reqObj,
+              type: '__GRPCWEB_DEVTOOLS__',
+            },
+            '*'
+          );
+        }
+      } else if (error instanceof Error) {
+        window.postMessage(
+          {
+            error: {
+              code: 2,
+              message: `${error?.message || error}`,
+              name: error?.name,
+              stack: error?.stack,
+            },
+            method: path,
+            methodType: 'unary',
+            request: reqObj,
+            type: '__GRPCWEB_DEVTOOLS__',
+          },
+          '*'
+        );
+      }
+
+      throw error;
+    }
+  };
+
+export const devtoolsStreamLoggingMiddleware: ClientMiddleware<DevtoolsLoggingOptions> =
+  async function* devtoolsLoggingMiddleware<Request, Response>(
+    call: ClientMiddlewareCall<Request, Response>,
+    options: CallOptions & Partial<DevtoolsLoggingOptions>
+  ): AsyncGenerator<Response, Response | void, undefined> {
+    // skip unary calls
+    if (!call.responseStream && !call.requestStream) {
+      return yield* call.next(call.request, options);
     }
 
-    throw error;
-  }
-};
+    // log streaming calls
+    const { path } = call.method;
+    const reqObj = getAsObject(call.request);
 
-export const devtoolsLoggingMiddleware: ClientMiddleware = composeClientMiddleware(
+    let first = true;
+    try {
+      for await (const response of call.next(call.request, options)) {
+        const resObj = getAsObject(response);
+        if (first) {
+          // log the request object only once
+          window.postMessage(
+            {
+              method: path,
+              methodType: 'server_streaming',
+              request: reqObj,
+              type: '__GRPCWEB_DEVTOOLS__',
+            },
+            '*'
+          );
+          first = false;
+        }
+        // log the response
+        window.postMessage(
+          {
+            method: path,
+            methodType: 'server_streaming',
+            response: resObj,
+            type: '__GRPCWEB_DEVTOOLS__',
+          },
+          '*'
+        );
+
+        yield response;
+      }
+    } catch (error) {
+      if (error instanceof ClientError) {
+        window.postMessage(
+          {
+            error: {
+              code: error?.code,
+              message: `${error?.message || error}`,
+              name: error?.name,
+              stack: error?.stack,
+            },
+            method: path,
+            methodType: 'server_streaming',
+            type: '__GRPCWEB_DEVTOOLS__',
+          },
+          '*'
+        );
+      } else if (isAbortError(error) && error instanceof Error) {
+        if (!options.skipAbortErrorLogging) {
+          window.postMessage(
+            {
+              error: {
+                code: 1,
+                message: `${error?.message || error}`,
+                name: error?.name,
+                stack: error?.stack,
+              },
+              method: path,
+              methodType: 'server_streaming',
+              type: '__GRPCWEB_DEVTOOLS__',
+            },
+            '*'
+          );
+        }
+      } else if (error instanceof Error) {
+        window.postMessage(
+          {
+            error: {
+              code: 2,
+              message: `${error?.message || error}`,
+              name: error?.name,
+              stack: error?.stack,
+            },
+            method: path,
+            methodType: 'server_streaming',
+            type: '__GRPCWEB_DEVTOOLS__',
+          },
+          '*'
+        );
+      }
+
+      throw error;
+    }
+  };
+
+export const devtoolsLoggingMiddleware: ClientMiddleware<DevtoolsLoggingOptions> = composeClientMiddleware(
   devtoolsUnaryLoggingMiddleware,
   devtoolsStreamLoggingMiddleware
 );
+
+// check whether the given object has toObject() method and return the object
+// otherwise return the object itself
+function getAsObject(obj: any) {
+  if ('toObject' in obj && typeof obj.toObject === 'function') {
+    // google-protobuf
+    return obj.toObject();
+  }
+  // ts-proto
+  return obj;
+}
