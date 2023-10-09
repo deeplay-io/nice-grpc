df938784c92b00ffee3684512fb7f0cb3bd1809f
Sebastian Halder <sebastian.halder@gmx.de>
2023-10-09 05:34:21 +0000

    Add tests for devtools middleware

--- packages/nice-grpc-client-middleware-devtools/jest.config.js
+++ packages/nice-grpc-client-middleware-devtools/jest.config.js
@@ -1,6 +1,6 @@
 /** @type {import('@jest/types').Config.GlobalConfig} */
 module.exports = {
-  testPathIgnorePatterns: ['/node_modules/', '/lib/'],
+  testPathIgnorePatterns: ['/node_modules/', '/lib/', '/fixtures/'],
   preset: 'ts-jest',
   testEnvironment: 'node',
   testTimeout: 15000,
@@ -9,4 +9,9 @@ module.exports = {
   coverageDirectory: 'coverage',
   coverageReporters: ['lcov', 'text'],
   coveragePathIgnorePatterns: ['/node_modules/', '/lib/'],
+  globals: {
+    window: {
+      postMessage: () => ({}),
+    },
+  },
 };
--- packages/nice-grpc-client-middleware-devtools/src/index.test.ts
+++ packages/nice-grpc-client-middleware-devtools/src/index.test.ts
@@ -8,7 +8,120 @@ import {
   Status,
 } from 'nice-grpc';
 import {devtoolsLoggingMiddleware} from '.';
-import {TestService} from '../fixtures/test_grpc_pb';
-import {TestRequest, TestResponse} from '../fixtures/test_pb';
+import {TestService} from '../fixtures/grpc-js/test_grpc_pb';
+import {
+  TestDefinition,
+  TestRequest as TestRequestTS,
+} from '../fixtures/ts-proto/test';
+import {
+  TestRequest as TestRequestJS,
+  TestResponse as TestResponseJS,
+} from '../fixtures/grpc-js/test_pb';
 
-// TODO: Can this middleware be tested?
+function throwUnimplemented(): never {
+  throw new ServerError(Status.UNIMPLEMENTED, '');
+}
+
+let windowSpy: jest.SpyInstance;
+let postMessageMock: jest.Mock;
+
+beforeEach(() => {
+  postMessageMock = jest.fn();
+  windowSpy = jest.spyOn(window, 'postMessage');
+  windowSpy.mockImplementation(postMessageMock);
+});
+
+afterEach(() => {
+  windowSpy.mockRestore();
+});
+
+describe('devtools', () => {
+  test('grpc-js logs unary calls', async () => {
+    const server = createServer();
+
+    server.add(TestService, {
+      async testUnary(request: TestRequestJS, {signal}) {
+        return new TestResponseJS();
+      },
+      testServerStream: throwUnimplemented,
+      testClientStream: throwUnimplemented,
+      testBidiStream: throwUnimplemented,
+    });
+
+    const port = await server.listen('localhost:0');
+
+    const channel = createChannel(`localhost:${port}`);
+    const client = createClientFactory()
+      .use(devtoolsLoggingMiddleware)
+      .create(TestService, channel);
+
+    const req = new TestRequestJS();
+    req.setId('test-id');
+
+    const promise = client.testUnary(req);
+
+    await expect(promise).resolves.toEqual(new TestResponseJS());
+    await expect(postMessageMock).toHaveBeenCalledTimes(1);
+    await expect(postMessageMock).toHaveBeenCalledWith(
+      expect.objectContaining({
+        request: {
+          id: 'test-id',
+        },
+        response: {
+          id: '',
+        },
+        methodType: 'unary',
+        method: '/nice_grpc.test.Test/TestUnary',
+      }),
+      '*',
+    );
+
+    channel.close();
+
+    await server.shutdown();
+  });
+
+  test('ts-proto logs unary calls', async () => {
+    const server = createServer();
+
+    server.add(TestService, {
+      async testUnary(request: TestRequestJS, {signal}) {
+        return new TestResponseJS();
+      },
+      testServerStream: throwUnimplemented,
+      testClientStream: throwUnimplemented,
+      testBidiStream: throwUnimplemented,
+    });
+
+    const port = await server.listen('localhost:0');
+
+    const channel = createChannel(`localhost:${port}`);
+    const client = createClientFactory()
+      .use(devtoolsLoggingMiddleware)
+      .create(TestDefinition, channel);
+
+    const req: TestRequestTS = {id: 'test-id'};
+
+    const promise = client.testUnary(req);
+
+    await expect(promise).resolves.toEqual({id: ''});
+    await expect(postMessageMock).toHaveBeenCalledTimes(1);
+    await expect(postMessageMock).toHaveBeenCalledWith(
+      expect.objectContaining({
+        request: {
+          id: 'test-id',
+        },
+        response: {
+          id: '',
+        },
+        methodType: 'unary',
+        method: '/nice_grpc.test.Test/TestUnary',
+      }),
+      '*',
+    );
+
+    channel.close();
+
+    await server.shutdown();
+  });
+});
