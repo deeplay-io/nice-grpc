a18c12a1e5f970221349a7f192a1a5aca23cb9ba
aikoven <aikoven@deeplay.io>
2023-10-19 16:09:07 +0200

    test(nice-grpc-client-middleware-devtools): change localhost to 127.0.0.1
    To fix potential ipv6 issues

--- packages/nice-grpc-client-middleware-devtools/src/index.test.ts
+++ packages/nice-grpc-client-middleware-devtools/src/index.test.ts
@@ -48,9 +48,9 @@ describe('devtools', () => {
       testBidiStream: throwUnimplemented,
     });
 
-    const port = await server.listen('localhost:0');
+    const port = await server.listen('127.0.0.1:0');
 
-    const channel = createChannel(`localhost:${port}`);
+    const channel = createChannel(`127.0.0.1:${port}`);
     const client = createClientFactory()
       .use(devtoolsLoggingMiddleware)
       .create(TestService, channel);
@@ -93,9 +93,9 @@ describe('devtools', () => {
       testBidiStream: throwUnimplemented,
     });
 
-    const port = await server.listen('localhost:0');
+    const port = await server.listen('127.0.0.1:0');
 
-    const channel = createChannel(`localhost:${port}`);
+    const channel = createChannel(`127.0.0.1:${port}`);
     const client = createClientFactory()
       .use(devtoolsLoggingMiddleware)
       .create(TestDefinition, channel);
